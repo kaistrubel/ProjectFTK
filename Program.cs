fe90c8c30b0613110e3d9c51b5679542eef39553
Philip Edat <philipedat@gmail.com>
2022-06-02 23:03:48 -0700

    try account fallback react with dev not doing that

--- Program.cs
+++ Program.cs
@@ -34,35 +34,42 @@ builder.Services.AddAzureClients(cfg =>
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+/*
 builder.Services.AddSpaStaticFiles(configuration => {
         configuration.RootPath = "ReactApp/build";
     });
-
+*/
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
+    app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"); //rm =Home for react
 }
 else
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+
+    app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action}/{id?}"); //rm =Home for react
 }
+
+app.MapRazorPages();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-app.UseSpaStaticFiles();
+//app.UseSpaStaticFiles();
 app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action}/{id?}"); //rm =Home for react
-app.MapRazorPages();
+
 
 /*
 app.UseSpa(spa =>
