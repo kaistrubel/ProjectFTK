6080ddcec5b5cd6be81f8acb13d00fa59bee87a1
Philip Edat <philipedat@gmail.com>
2022-06-25 11:41:23 -0700

    initial sign in, need to overlay

--- ReactApp/src/components/SignUp/index.tsx
+++ ReactApp/src/components/SignUp/index.tsx
@@ -2,16 +2,32 @@ import teacherPng from '../../assets/images/teacher_signin.png';
 import studentPng from '../../assets/images/student_signin.png'; 
 
 const SignUp = () => (
-    <div className="grid place-items-center">
+    <div className="grid place-items-center pt-16">
       <div className="bubble bubble-header">
         Welcome to <strong>Project FTK.</strong>
       </div>
-      <div className="container py-16 flex items-start justify-between flex-col lg:flex-row" id="student">
-        <div className="flex-1 w-full md:w-1/2">
+      <div className="container flex items-start justify-between flex-col lg:flex-row" id="signup">
+        <div className="flex-1 w-full lg:w-1/2">
+          <div className="grid place-items-center">
+            <div className="bubble">
+              Teachers
+            </div>
           <img src={teacherPng} className="comic-border object-cover h-96 w-192" alt="Teacher Signup" />
+          <button type="button" className="bubble bubble--highlight" >
+              Sign in with Google
+            </button>
+          </div>
         </div>
-        <div className="flex-1 w-full md:w-1/2">
-          <img src={studentPng} className="comic-border comic-border-no-left object-cover h-96 w-192" alt="Student Signup" />
+        <div className="flex-1 w-full lg:w-1/2">
+          <div className="grid place-items-center">
+            <div className="bubble">
+              Students
+            </div>
+            <img src={studentPng} className="comic-border object-cover h-96 w-192" alt="Student Signup" />
+            <button type="button" className="bubble bubble--highlight" >
+              Sign in with Google
+            </button>
+          </div>
         </div>
       </div>
     </div>
