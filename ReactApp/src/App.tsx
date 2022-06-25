622b4ac4f447a08e246308ded951f7c62a31ca5d
Philip Edat <philipedat@gmail.com>
2022-06-24 18:34:51 -0700

    start fe

--- ReactApp/src/App.tsx
+++ ReactApp/src/App.tsx
@@ -1,24 +1,20 @@
 ﻿import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Yo yo yo, we got some react in this bitch. If I knew any front end, I would've hooked up google auth here lol
-        </p>
-        <a
-          className="App-link"
-          href="https://projectftk.com/Home/Index"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Try logging in here with my sick backend UI!!
-        </a>
-      </header>
+      <div className="bubble">
+        Welcome to <strong>Project FTK.</strong>
+      </div>
+      <a
+        className="bubble bubble--highlight"
+        href="https://projectftk.com/Home/Index"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Login via Backend
+      </a>
     </div>
   );
 }
