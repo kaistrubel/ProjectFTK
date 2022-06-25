8d2a76c21a4d8899bc778d6bc33dcea61517ef88
Philip Edat <philipedat@gmail.com>
2022-06-25 10:20:43 -0700

    use tailwind

--- ReactApp/src/components/SignUp/index.tsx
+++ ReactApp/src/components/SignUp/index.tsx
@@ -2,15 +2,17 @@ import teacherPng from '../../assets/images/teacher_signin.png';
 import studentPng from '../../assets/images/student_signin.png'; 
 
 const SignUp = () => (
-    <div className="container py-16 flex items-start justify-between flex-col lg:flex-row" id="student">
-      <div className="flex-1 w-full md:w-1/2 border-2 border-white">
-        <img src={teacherPng} className="comic-border" alt="Teacher Signup" />
-        <div className="bubble" style={{alignContent : "center"}}>
-          Welcome to <strong>Project FTK.</strong>
-        </div>
+    <div className="grid place-items-center">
+      <div className="bubble bubble-header">
+        Welcome to <strong>Project FTK.</strong>
       </div>
-      <div className="flex-1 w-full md:w-1/2 border-2 border-white">
-        <img src={studentPng} className="comic-border" alt="Student Signup" />
+      <div className="container py-16 flex items-start justify-between flex-col lg:flex-row" id="student">
+        <div className="flex-1 w-full md:w-1/2">
+          <img src={teacherPng} className="comic-border object-cover h-96 w-192" alt="Teacher Signup" />
+        </div>
+        <div className="flex-1 w-full md:w-1/2">
+          <img src={studentPng} className="comic-border comic-border-no-left object-cover h-96 w-192" alt="Student Signup" />
+        </div>
       </div>
     </div>
 );
