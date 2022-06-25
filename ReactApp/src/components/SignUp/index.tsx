31cb5afbc63d679c15345c14c9f9c0f10b766f48
Philip Edat <philipedat@gmail.com>
2022-06-25 12:09:24 -0700

    sign in possibly working

--- ReactApp/src/components/SignUp/index.tsx
+++ ReactApp/src/components/SignUp/index.tsx
@@ -1,5 +1,5 @@
 import teacherPng from '../../assets/images/teacher_signin.png'; 
-import studentPng from '../../assets/images/student_signin.png'; 
+import studentPng from '../../assets/images/student_signin.png';
 
 const SignUp = () => (
     <div className="grid place-items-center pt-16">
@@ -12,10 +12,11 @@ const SignUp = () => (
             <div className="bubble">
               Teachers
             </div>
-          <img src={teacherPng} className="comic-border object-cover h-96 w-192" alt="Teacher Signup" />
-          <button type="button" className="bubble bubble--highlight" >
-              Sign in with Google
-            </button>
+          <img src={teacherPng} className="comic-border object-cover h-96 w-192" style={{zIndex:-10}} alt="Teacher Signup" />
+          <a
+              className="bubble bubble--highlight"
+              href="https://projectftk.com/Auth/GoogleLogin?isTeacher=true"
+            >Sign in with Google</a>
           </div>
         </div>
         <div className="flex-1 w-full lg:w-1/2">
@@ -23,10 +24,11 @@ const SignUp = () => (
             <div className="bubble">
               Students
             </div>
-            <img src={studentPng} className="comic-border object-cover h-96 w-192" alt="Student Signup" />
-            <button type="button" className="bubble bubble--highlight" >
-              Sign in with Google
-            </button>
+            <img src={studentPng} className="comic-border object-cover h-96 w-192" style={{zIndex:-10}} alt="Student Signup" />
+            <a
+              className="bubble bubble--highlight"
+              href="https://projectftk.com/Auth/GoogleLogin"
+            >Sign in with Google</a>
           </div>
         </div>
       </div>
