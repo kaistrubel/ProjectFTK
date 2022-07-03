936bc039e7e1e9dbd44d114c07274eafd33c21b0
Philip Edat <philipedat@gmail.com>
2022-07-03 08:47:44 -0700

    signup css decent

--- ReactApp/src/components/SignUp/index.tsx
+++ ReactApp/src/components/SignUp/index.tsx
@@ -48,10 +48,10 @@ const SignUp = () => {
         <div className="bubble bubble-header">
           Welcome to <strong>Project FTK.</strong>
         </div>
-        <div className="container flex items-start justify-between flex-col lg:flex-row" id="signup">
-          <div className="flex-1 w-full lg:w-1/2 bg-[url('assets/images/teacher_signin.png')] comic-border bg-cover h-96 w-384">
-          <div className="grid grid-rows-5 place-items-center">
-            <div className="row-span-1 title">Teachers</div>
+        <div className="container flex items-start justify-between flex-col lg:flex-row signup" id="signup">
+          <div className="flex-1 w-full lg:w-1/2 bg-[url('assets/images/teacher_signin.png')] comic-border bg-cover lg:h-96">
+          <div className="grid grid-rows-[100px_minmax(300px,_1fr)_100px] place-items-center">
+            <div className="title">Teachers</div>
             <div className="row-span-4">
               <a
                 className="bubble bubble--highlight"
@@ -61,8 +61,8 @@ const SignUp = () => {
           </div>
 
           </div>
-          <div className="flex-1 w-full lg:w-1/2 bg-[url('assets/images/student_signin.png')] comic-border bg-cover h-96 w-384">
-            <div className="grid grid-rows-5 place-items-center">
+          <div className="flex-1 w-full lg:w-1/2 bg-[url('assets/images/student_signin.png')] comic-border bg-cover lg:h-96">
+            <div className="grid grid-rows-[100px_minmax(300px,_1fr)_100px] place-items-center">
               <div className="row-span-1 title">Students</div>
               <div className="row-span-4 ...">
                   <a
