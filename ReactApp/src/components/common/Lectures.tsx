6a745f1c3f19e084401ffe7fbfb1721f1f5b9e1e
Philip Edat <philipedat@gmail.com>
2022-07-06 00:02:59 -0700

    fix proper teacher route from lecture

--- ReactApp/src/components/common/Lectures.tsx
+++ ReactApp/src/components/common/Lectures.tsx
@@ -1,17 +1,12 @@
-import { useEffect, useState } from "react";
-import ClassApi from "../../apis/class";
-import ICourse from "../../types/Course";
-import IUserInfo from "../../types/User";
 import NoClasses from "./NoClasses";
 import { Link, useNavigate } from 'react-router-dom';
-import CreateClass from "../teacher/CreateClass";
 
 const Lectures = (props: any) => {
   
     return(
       <>
         <div className="px-4 text-right sm:px-6">
-          <Link to= {props.user.isTeacher ==false? "/createClass": "/joinClass"} replace={true}>
+          <Link to= {props.user.isTeacher? "/createClass": "/joinClass"} replace={true}>
             <button
               type="submit"
               className="text-black bubble bubble--highlight hover:bg-indigo-700 hover:text-white"
--- ReactApp/src/components/student/JoinClass.tsx
+++ ReactApp/src/components/student/JoinClass.tsx
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react";
 import ClassApi from "../../apis/class";
-import ISubject from "../../types/Subject";
 import * as React from "react";
 import { useNavigate } from "react-router-dom";
 
