8a77d36ef51a1b46531571447912bee3d1c3f978
Philip Edat <philipedat@gmail.com>
2022-07-10 14:59:12 -0700

    mobile better navbar but browser not right

--- ReactApp/src/components/common/Navbar.tsx
+++ ReactApp/src/components/common/Navbar.tsx
@@ -67,12 +67,9 @@ export default function Navbar(props: any) {
                         {item.name}
                       </a>
                     ))}
-                  </div>
-                </div>
-              </div>
-              <div className="grid">
+
                 <Listbox value={props.selectedCourse} onChange={props.setSelectedCourse}>
-                  <div className="relative mt-1">
+                  <div className="absolute">
                     <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
                       <span className="block truncate">{props.selectedCourse?.displayName}</span>
                       <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
@@ -121,7 +118,11 @@ export default function Navbar(props: any) {
                     </Transition>
                   </div>
                 </Listbox>
+                    
+                  </div>
+                </div>
               </div>
+              
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 {/* Profile dropdown */}
                 <Menu as="div" className="ml-3 relative">
@@ -199,6 +200,58 @@ export default function Navbar(props: any) {
                   {item.name}
                 </Disclosure.Button>
               ))}
+
+                <Listbox value={props.selectedCourse} onChange={props.setSelectedCourse}>
+                  <div className="relative mt-1">
+                    <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                      <span className="block truncate">{props.selectedCourse?.displayName}</span>
+                      <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                        <SelectorIcon
+                          className="h-5 w-5 text-gray-400"
+                          aria-hidden="true"
+                        />
+                      </span>
+                    </Listbox.Button>
+                    <Transition
+                      as={Fragment}
+                      leave="transition ease-in duration-100"
+                      leaveFrom="opacity-100"
+                      leaveTo="opacity-0"
+                    >
+                      <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm dropdown">
+                        {props.courses.map((course: ICourse, courseIdx: number) => (
+                          <Listbox.Option
+                            key={courseIdx}
+                            className={({ active }) =>
+                              `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                                active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
+                              }`
+                            }
+                            value={course}
+                          >
+                            {({ selected }) => (
+                              <>
+                                <span
+                                  className={`block truncate ${
+                                    selected ? 'font-medium' : 'font-normal'
+                                  }`}
+                                >
+                                  {course?.displayName}
+                                </span>
+                                {selected ? (
+                                  <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                                    <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                  </span>
+                                ) : null}
+                              </>
+                            )}
+                          </Listbox.Option>
+                        ))}
+                      </Listbox.Options>
+                    </Transition>
+                  </div>
+                </Listbox>
+
             </div>
           </Disclosure.Panel>
         </>
