4ea096d2db4db7f48f9f550646a8307a5fef4975
Philip Edat <philipedat@gmail.com>
2022-07-21 00:47:03 -0700

    analysis w/ windmill kinda working. need to figure out css

--- ReactApp/src/components/teacher/Analysis.tsx
+++ ReactApp/src/components/teacher/Analysis.tsx
@@ -1,3 +1,4 @@
+import {TableContainer,Table,TableHeader,TableBody,TableRow,TableCell, Avatar, Badge, TableFooter, Pagination} from '@windmill/react-ui'
 import { useMemo, useState } from "react";
 import LessonApi from "../../apis/lesson";
 import { IStudentAnalysis } from "../../types/User";
@@ -19,7 +20,42 @@ const Analysis = (props: any) => {
 
     return (
       <>
-        <h1>ello world</h1>
+        <TableContainer>
+            <Table>
+                <TableHeader>
+                <TableRow>
+                    <TableCell>Student</TableCell>
+                    <TableCell>Current</TableCell>
+                    <TableCell>Status</TableCell>
+                    <TableCell>Time Spent</TableCell>
+                </TableRow>
+                </TableHeader>
+                <TableBody>
+                {analysis.map((student: IStudentAnalysis) => (
+                <TableRow>
+                    <TableCell>
+                        <div className="flex items-center text-sm">
+                            <Avatar src={student.pictureUrl} alt={student.name} />
+                            <span className="font-semibold ml-2">{student.name}</span>
+                        </div>
+                    </TableCell>
+                    <TableCell>
+                        <Badge type="success">{student.status}</Badge>
+                    </TableCell>
+                    <TableCell>
+                        <span className="text-sm">{student.current}</span>
+                    </TableCell>
+                    <TableCell>
+                        <span className="text-sm">{student.time}</span>
+                    </TableCell>
+                </TableRow>
+                ))}
+                </TableBody>
+            </Table>
+            <TableFooter>
+                <Pagination totalResults={10} resultsPerPage={4} onChange={() => {}} label="Table navigation" />
+            </TableFooter>
+        </TableContainer>
       </>
   );
 };
